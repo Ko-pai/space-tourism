193580dc14a755cf304b6d936d0646c3ebc235d7
Pai thet <leontinus866@gmail.com>
2023-04-23 16:40:37 +0630

    Backup branch first commit

--- scripts.js
+++ scripts.js
@@ -1,17 +1,35 @@
 const textContainer = document.querySelector("ul")
-const main = document.querySelector("main")
+const main = document.querySelector("body")
 const homeAddContent = `
 <div class="homePageLeftSize">
                 <div class="innerLeft">
                     <p class="pText0">SO, YOU WANT TO TRAVEL TO</p>
                     <h1>SPACE</h1>
-                    <p class="pText">Let’s face it; if you want to go to space, you might as well genuinely go to outer space and not hover kind of on the edge of it. Well sit back, and relax because we’ll give you a truly out of this world experience! Explore</p>
+                    <p class="pText">Let’s face it; if you want to go to space, you might as well genuinely go to outer space and not hover kind of on the edge of it. Well sit back, and relax because we’ll give you a truly out of this world experience!</p>
                 </div>
             </div>
             <div class="homePageRightSize">
                 <div class="circle">EXPLORE</div>
             </div>
 `
+const destinationAddContent = `
+<div class="homePageLeftSize">
+                <div class="innerLeft">
+                    <p class="pText0">SO, YOU WANT TO TRAVEL TO</p>
+                    <h1>Destina</h1>
+                    <p class="pText">Let’s face it; if you want to go to space, you might as well genuinely go to outer space and not hover kind of on the edge of it. Well sit back, and relax because we’ll give you a truly out of this world experience!</p>
+                </div>
+            </div>
+            <div class="homePageRightSize">
+                <div class="circle">EXPLORE</div>
+            </div>
+`
+
+const bgImage = [
+    {size : 550 , url : `url("assets/home/background-home-mobile.jpg")`},
+    {size : 1400 , url : `url("assets/home/background-home-desktop.jpg")`}
+
+]
 const list = [
     {
         num: "00",
@@ -33,53 +51,77 @@ const list = [
 
 for(let i = 0 ; i < list.length ; i++){
     const liTag = document.createElement("li")
+    const link = document.createElement("a")
     const spanTag = document.createElement("span")
     const slider = document.querySelector(".slider")
     const good = document.querySelector(".second-container")
     
     
-    liTag.id = i
+    link.id = i
     spanTag.append(list[i].num)
-    liTag.append(spanTag)
-    liTag.append(list[i].name)
+    link.append(spanTag)
+    link.append(list[i].name)
+    liTag.append(link)
     textContainer.append(liTag)
-  
-    if(i == 0){
-        slider.style.width = liTag.offsetWidth + "px"
-        good.style.backgroundColor = "transparent"
-        good.innerHTML = homeAddContent
-       
+    
+    function myfunc (){
+        const screenWidth = window.innerWidth
+
+        const backImg = bgImage.find(image => image.size <= screenWidth)
+        const takeImg = bgImage.find(image => image.size > screenWidth)
         
+            main.style.background = backImg.url
+            main.style.backgroundSize = "cover"
+       
+    }
+    function myfunc1 (){
         if(window.innerWidth < 550){
             main.style.background = `url("assets/home/background-home-mobile.jpg")`
             main.style.backgroundSize = "cover"
             console.log(window.innerWidth);
         }else{
-
             main.style.background = `url("assets/home/background-home-desktop.jpg")`
             main.style.backgroundSize = "cover"
         }
     }
+
+
+    function callWindow (){
+        window.addEventListener("load",myfunc1)
+        window.addEventListener("resize",myfunc1)
+    }
+    callWindow()
+
+
+    if(i == 0){
+        slider.style.width = liTag.offsetWidth + "px"
+        good.style.backgroundColor = "transparent"
+        good.innerHTML = homeAddContent
+       callWindow()
+    }
     liTag.addEventListener("click",(e)=>{
             let leftMove = e.target.offsetLeft
             let width = e.target.offsetWidth
+            let idNum = e.target.id
+            let atag = document.getElementById(idNum)
+            console.log(atag);
             slider.style.width = width + "px"
             slider.style.transform = `translateX(${leftMove}px)`
         
            if(liTag.id == 1){
+                
                 main.style.background = `url("assets/destination/background-destination-desktop.jpg")`
                 main.style.backgroundSize = "cover"
                 good.innerHTML =''
             }else if(liTag.id == 0){
-                good.style.backgroundColor = "transparent"
-                main.style.background = `url("assets/home/background-home-desktop.jpg")`
-                main.style.backgroundSize = "cover"
+                myfunc1()
                 good.innerHTML = homeAddContent
+                
             }else if(liTag.id == 2){
-                good.style.backgroundColor = "transparent"
                 main.style.background = `url("assets/crew/background-crew-desktop.jpg")`
                 main.style.backgroundSize = "cover"
                 good.innerHTML =''
+                
 
             }else{
                 
